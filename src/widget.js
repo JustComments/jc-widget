64062bc96f1345d8cb1a4a4ece58263ea53cac3a
Alex R <alexei.rudenko@gmail.com>
2019-12-13 13:55:10 +0100

    Fix show display

--- src/widget.js
+++ src/widget.js
@@ -90,7 +90,7 @@ class Widget extends Component {
               {countText}
             </span>
             <span className={cls(s.fontBody2)}>
-              {__('show')}{' '}
+              {__('show', customLocale)}{' '}
               <select
                 onChange={onSortChange}
                 className={cls(s.select, s.fontBody2)}
